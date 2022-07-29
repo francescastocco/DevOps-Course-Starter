c76d3afd8d7fe1c4308b02026af0e16171947519
Francesca Stocco <francesca.stocco@softwire.com>
2022-07-29 16:52:31 +0100

    Remove delete route from app.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item, delete_item
+from todo_app.data.session_items import get_items, add_item
 
 
 app = Flask(__name__)
@@ -17,10 +17,4 @@ def index():
 def create():
     newTaskTitle = request.form.get('task-title')
     add_item(newTaskTitle)
-    return redirect(url_for('index'))
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    index = 0
-    delete_item(index)
     return redirect(url_for('index'))
