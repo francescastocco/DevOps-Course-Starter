21d2901c330541d1efbee992f82af1c7a2498293
Francesca Stocco <francesca.stocco@softwire.com>
2022-09-28 16:41:41 +0100

    exercise-2: condition on status update

--- todo_app/app.py
+++ todo_app/app.py
@@ -16,13 +16,13 @@ def index():
 
 @app.route('/create', methods = ['POST'])
 def create():
-    newTaskTitle = request.form.get('task-title')
-    create_card(newTaskTitle)
+    new_card_title = request.form.get('card-title')
+    create_card(new_card_title)
     return redirect(url_for('index'))
 
-@app.route('/update', methods = ['PUT'])
+@app.route('/update', methods = ['POST'])
 def update():
-    card_id = 
-    card_status =
+    card_id = request.form.get('card-id')
+    card_status = request.form.get('card-status')
     update_card_status(card_id, card_status)
     return redirect(url_for('index'))
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,21 +2,12 @@ import os
 import requests
 
 
-def get_board():
-    base_url = f'https://api.trello.com/1/boards/{os.getenv("BOARD_ID")}'
-    query_params = {
-        "key": {os.getenv("API_KEY")},
-        "token": {os.getenv("API_TOKEN")}
-    }
-    response = requests.get(base_url, params = query_params)
-    return response.json()
-
 def get_all_cards():
     base_url = f'https://api.trello.com/1/boards/{os.getenv("BOARD_ID")}/lists'
     query_params = {
-        "key": {os.getenv("API_KEY")},
-        "token": {os.getenv("API_TOKEN")},
-        "cards": "open"
+        'key': {os.getenv('API_KEY')},
+        'token': {os.getenv('API_TOKEN')},
+        'cards': 'open'
     }
     response = requests.get(base_url, params = query_params)
     return response.json()
@@ -24,21 +15,27 @@ def get_all_cards():
 def create_card(title):
     base_url = 'https://api.trello.com/1/cards'
     query_params = {
-        "key": {os.getenv("API_KEY")},
-        "token": {os.getenv("API_TOKEN")},
-        "name": title,
-        "idList": {os.getenv("TO_DO_LIST_ID")}
+        'key': {os.getenv('API_KEY')},
+        'token': {os.getenv('API_TOKEN')},
+        'name': title,
+        'idList': {os.getenv('TO_DO_LIST_ID')}
     }
     response = requests.post(base_url, data = query_params)
     response.raise_for_status()
 
-def update_card_status(id, status):
-    base_url = f'https://api.trello.com/1/card/{id}'
-    list_id = {os.getenv("DONE_LSIT_ID")}
+def update_card_status(card_id, card_status):
+    base_url = f'https://api.trello.com/1/cards/{card_id}'
+    list_id = get_next_list_id(card_status)
     query_params = {
-        "key": {os.getenv("API_KEY")},
-        "token": {os.getenv("API_TOKEN")},
-        "idList": list_id
+        'key': {os.getenv('API_KEY')},
+        'token': {os.getenv('API_TOKEN')},
+        'idList': list_id
     }
     response = requests.put(base_url, data = query_params)
     response.raise_for_status()
+
+def get_next_list_id(card_status):
+    if card_status == str(os.getenv("TO_DO_LIST_ID")):
+        return os.getenv("DOING_LIST_ID")
+    else:
+        return os.getenv("DONE_LIST_ID")
