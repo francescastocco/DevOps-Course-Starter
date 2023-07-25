2e4bdada7d3d998160f9dc2b16bfea9dbb39fa11
Francesca Stocco <francescastocco@pvh.com>
2023-07-25 14:43:16 +0100

    exercise-11: fix error in decorator

--- todo_app/data/decorators/user_role_decorator.py
+++ todo_app/data/decorators/user_role_decorator.py
@@ -7,7 +7,7 @@ import flask_login
 def requires_write_access(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not flask_login.current_user.has_write_access():
+        if not flask_login.current_user.has_write_access:
             abort(403)
         return f(*args, **kwargs)
     return decorated_function
